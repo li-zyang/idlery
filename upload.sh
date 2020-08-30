4f82ed9837d70668075d699eb802b5dda2833ca0
li-zhaoyang <K_AEIx@163.com>
2020-08-30 23:08:15 +0800

    Renamed update.sh to upload.sh; Modified upload.sh to only add content-related files

--- upload.sh
+++ upload.sh
@@ -2,6 +2,9 @@ set -e
 set -o pipefail
 python ./utils/genthumbnail.py
 python ./utils/fillupload.py README.md
-git add .
+git add README.md
+git add covers
+git add src
+git add thumbnail
 git commit -m 'common update'
 git push
